74c265220e3996426521443dd940bb079b2ed777
sumitssaurabh <117895961+sumitssaurabh@users.noreply.github.com>
2023-02-27 00:09:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
 //your code here
-function swapTheme() {
-  if (appDiv.classList.contains('day')) {
-    // switch to night theme
-    appDiv.classList.remove('day');
-    appDiv.classList.add('night');
-    swapButton.textContent = 'Switch to Day Theme';
-    swapButton.classList.remove('button_day');
-    swapButton.classList.add('button_night');
-  } else {
-    // switch to day theme
-    appDiv.classList.remove('night');
-    appDiv.classList.add('day');
-    swapButton.textContent = 'Switch to Night Theme';
-    swapButton.classList.remove('button_night');
-    swapButton.classList.add('button_day');
-  }
+let button=document.getElementById('swap');
+let body=document.getElementsByTagName('body')
+let main=document.getElementById("app")
+
+function swap(){
+    //body.style.backgroundcolor="dark";
+    // var element = document.body;
+    // element.classList.toggle("dark-mode");
+    if(button.getAttribute("class")==="button_day"){
+    document.body.style.backgroundColor = "dark";
+    button.setAttribute("class", "button_night")
+    main.setAttribute("class","night")
+    }
+    else{
+        document.body.style.backgroundColor = "light";
+        button.setAttribute("class", "button_day")
+        main.setAttribute("class","day") 
+    }
+
+    //console.log("hi")
 }
-
-// add a click event listener to the swap button
-swapButton.addEventListener('click', swapTheme);
-Make sure you include this code inside a <script> tag at the end of your HTML file, after the elements you want to modify. Also, ensure the CSS classes 'day', 'night', 'button_day', and 'button_night' are defined properly in your CSS file, to ensure the styles get applied correctly.
-
-Let me know if you need any further assistance!
-
-
-
-
-Regenerate response
